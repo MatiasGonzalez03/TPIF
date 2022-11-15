62994b2908a5716b518f3b2c0f9c7477e3d15a73
MatiasGonzalez03 <matiasgonzalezezequiel92@gmail.com>
2022-11-15 02:51:48 -0300

    Cambios

--- PasoUno/app.py
+++ PasoUno/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # conexion a mysql
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'cacaguate'
 app.config['MYSQL_DB'] = 'flaskcontacts'
 mysql = MySQL(app)
 

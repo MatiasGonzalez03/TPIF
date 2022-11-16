5f82007859788e583af391b6b9c27858d4110db7
MatiasGonzalez03 <matiasgonzalezezequiel92@gmail.com>
2022-11-16 01:05:34 -0300

    Backend

--- ModuloLogin/src/config.py
+++ ModuloLogin/src/config.py
@@ -1,6 +1,15 @@
-class DevelopmentConfig():
-    DEBUG=True
+class Config:
+    SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
 
-config={
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'cacaguate'
+    MYSQL_DB = 'flask_login'
+
+
+config = {
     'development': DevelopmentConfig
 }
